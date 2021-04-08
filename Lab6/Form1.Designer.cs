c5bd6c9cb2d21656f55eab46c8fb0aae74b9c0a5
Гребенкин Алексей <24gas2001@gmail.com>
2021-04-08 19:23:05 +0700

    Рисует графики

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -46,6 +46,7 @@ namespace Lab6
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
@@ -54,13 +55,14 @@ namespace Lab6
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(501, 107);
+            this.label7.Location = new System.Drawing.Point(499, 33);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(414, 51);
             this.label7.TabIndex = 41;
@@ -69,7 +71,7 @@ namespace Lab6
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(510, 174);
+            this.textBox1.Location = new System.Drawing.Point(508, 100);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(405, 44);
             this.textBox1.TabIndex = 40;
@@ -78,7 +80,7 @@ namespace Lab6
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(529, 236);
+            this.button1.Location = new System.Drawing.Point(527, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(360, 52);
             this.button1.TabIndex = 39;
@@ -88,64 +90,112 @@ namespace Lab6
             // 
             // numericUpDown7
             // 
+            this.numericUpDown7.DecimalPlaces = 3;
             this.numericUpDown7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown7.Location = new System.Drawing.Point(102, 294);
+            this.numericUpDown7.Location = new System.Drawing.Point(110, 287);
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown7.Name = "numericUpDown7";
             this.numericUpDown7.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown7.TabIndex = 38;
             // 
             // numericUpDown8
             // 
+            this.numericUpDown8.DecimalPlaces = 3;
             this.numericUpDown8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown8.Location = new System.Drawing.Point(240, 294);
+            this.numericUpDown8.Location = new System.Drawing.Point(248, 287);
+            this.numericUpDown8.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown8.Name = "numericUpDown8";
             this.numericUpDown8.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown8.TabIndex = 37;
             // 
             // numericUpDown5
             // 
+            this.numericUpDown5.DecimalPlaces = 3;
             this.numericUpDown5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown5.Location = new System.Drawing.Point(102, 234);
+            this.numericUpDown5.Location = new System.Drawing.Point(110, 227);
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown5.Name = "numericUpDown5";
             this.numericUpDown5.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown5.TabIndex = 36;
             // 
             // numericUpDown6
             // 
+            this.numericUpDown6.DecimalPlaces = 3;
             this.numericUpDown6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown6.Location = new System.Drawing.Point(240, 234);
+            this.numericUpDown6.Location = new System.Drawing.Point(248, 227);
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown6.Name = "numericUpDown6";
             this.numericUpDown6.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown6.TabIndex = 35;
             // 
             // numericUpDown3
             // 
+            this.numericUpDown3.DecimalPlaces = 3;
             this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown3.Location = new System.Drawing.Point(102, 171);
+            this.numericUpDown3.Location = new System.Drawing.Point(110, 164);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown3.TabIndex = 34;
             // 
             // numericUpDown4
             // 
+            this.numericUpDown4.DecimalPlaces = 3;
             this.numericUpDown4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown4.Location = new System.Drawing.Point(240, 171);
+            this.numericUpDown4.Location = new System.Drawing.Point(248, 164);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown4.Name = "numericUpDown4";
             this.numericUpDown4.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown4.TabIndex = 33;
             // 
             // numericUpDown2
             // 
+            this.numericUpDown2.DecimalPlaces = 3;
             this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown2.Location = new System.Drawing.Point(240, 107);
+            this.numericUpDown2.Location = new System.Drawing.Point(248, 100);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown2.TabIndex = 32;
             // 
             // numericUpDown1
             // 
+            this.numericUpDown1.DecimalPlaces = 3;
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown1.Location = new System.Drawing.Point(102, 107);
+            this.numericUpDown1.Location = new System.Drawing.Point(110, 100);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 44);
             this.numericUpDown1.TabIndex = 31;
@@ -154,7 +204,7 @@ namespace Lab6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(41, 300);
+            this.label6.Location = new System.Drawing.Point(49, 293);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(35, 37);
             this.label6.TabIndex = 30;
@@ -164,7 +214,7 @@ namespace Lab6
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(41, 236);
+            this.label5.Location = new System.Drawing.Point(49, 229);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 37);
             this.label5.TabIndex = 29;
@@ -174,7 +224,7 @@ namespace Lab6
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(41, 171);
+            this.label4.Location = new System.Drawing.Point(49, 164);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 37);
             this.label4.TabIndex = 28;
@@ -184,7 +234,7 @@ namespace Lab6
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(41, 107);
+            this.label3.Location = new System.Drawing.Point(49, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(33, 37);
             this.label3.TabIndex = 27;
@@ -194,7 +244,7 @@ namespace Lab6
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(285, 54);
+            this.label2.Location = new System.Drawing.Point(293, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(32, 37);
             this.label2.TabIndex = 26;
@@ -204,17 +254,26 @@ namespace Lab6
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(141, 54);
+            this.label1.Location = new System.Drawing.Point(149, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 37);
             this.label1.TabIndex = 25;
             this.label1.Text = "x";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(560, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 42;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 450);
+            this.ClientSize = new System.Drawing.Size(1010, 545);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -242,6 +301,7 @@ namespace Lab6
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -266,6 +326,7 @@ namespace Lab6
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,9 +14,11 @@ namespace Lab6
     public partial class Form1 : Form
     {
         Figure figure;
+        Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,11 +39,70 @@ namespace Lab6
                 numericUpDown7.Value = (decimal)point4.X;
                 numericUpDown8.Value = (decimal)point4.Y;
                 textBox1.Text = figure.GetTypeFigure().ToString();
+
             }
             catch (Exception ex)
             {
                 textBox1.Text = ex.Message;
             }
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                //Thread.Sleep(100);
+
+                PointF[] pointsf = new PointF[4];
+                pointsf[0] = new PointF((float)numericUpDown1.Value , (float)numericUpDown2.Value);
+                pointsf[1] = new PointF((float)numericUpDown3.Value , (float)numericUpDown4.Value);
+                pointsf[2] = new PointF((float)numericUpDown5.Value , (float)numericUpDown6.Value);
+                pointsf[3] = new PointF((float)numericUpDown7.Value , (float)numericUpDown8.Value);
+
+
+                float minX = Math.Min(pointsf[0].X, pointsf[1].X);
+                minX = Math.Min(minX, pointsf[2].X);
+                minX = Math.Min(minX, pointsf[3].X);
+
+                float minY = Math.Min(pointsf[0].Y, pointsf[1].Y);
+                minY = Math.Min(minX, pointsf[2].Y);
+                minY = Math.Min(minX, pointsf[3].Y);
+
+                if (minX<0)
+                {
+                    for (int i = 0; i < pointsf.Length; i++)
+                        pointsf[i].X -= minX;
+                }
+                if (minY < 0)
+                {
+                    for (int i = 0; i < pointsf.Length; i++)
+                        pointsf[i].Y -= minY;
+                }
+
+                float maxX = Math.Max(pointsf[0].X, pointsf[1].X);
+                maxX = Math.Max(maxX, pointsf[2].X);
+                maxX = Math.Max(maxX, pointsf[3].X);
+
+                float maxY = Math.Max(pointsf[0].Y, pointsf[1].Y);
+                maxY = Math.Max(maxY, pointsf[2].Y);
+                maxY = Math.Max(maxY, pointsf[3].Y);
+
+                float k1 = pictureBox1.ClientSize.Width / maxX;
+                float k2 = pictureBox1.ClientSize.Height / maxY;
+
+                float k = Math.Min(k1, k2);
+
+                for (int i = 0; i < pointsf.Length; i++)
+                {
+                    pointsf[i].X *= k;
+                    pointsf[i].Y *= k;
+                }
+
+                g.Clear(Color.Transparent);
+
+                //g.DrawPolygon(Pens.Black, pointsf);
+                g.FillPolygon(Brushes.Red, pointsf);
+                    //g.DrawLine(Pens.Black, 10, 10, 40, 40);
+            }
+            /// Назначаем наш Bitmap свойству Image
+            pictureBox1.Image = bmp;
         }
     }
 }
