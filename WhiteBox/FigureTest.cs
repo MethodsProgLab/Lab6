781479c9d2449ac4fd5e97c0f5a5849b9f02bf64
Igor Anufriev <sacmi@pm.me>
2021-04-21 20:30:52 +0700

    белый ящик BloodTrail

--- WhiteBox/FigureTest.cs
+++ WhiteBox/FigureTest.cs
@@ -1,3 +1,4 @@
+using FigureLab;
 using NUnit.Framework;
 
 namespace WhiteBox
@@ -5,9 +6,126 @@ namespace WhiteBox
     public class FigureTest
     {
         [Test]
-        public void Test1()
+        public void PathForOneLine()
         {
-            Assert.Pass();
+            var points = new Point[4];
+            for (var i = 0; i < 4; i++)
+            {
+                points[i] = new Point(i, 0);
+            }
+
+            Assert.Catch(() => new Figure(points));
+
+            for (var i = 0; i < 4; i++)
+            {
+                points[i] = new Point(0, i);
+            }
+
+            Assert.Catch(() => new Figure(points));
+        }
+        
+        [Test]
+        public void PathForGeneralQuadrilateral()
+        {
+            var points = new Point[4];
+            points[0] = new Point(4, 0);
+            points[1] = new Point(-1, -7);
+            points[2] = new Point(-3, -7);
+            points[3] = new Point(-4, -2);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.GeneralQuadrilateral, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForGeneralTrapezoid()
+        {
+            var points = new Point[4];
+            points[0] = new Point(1, 1);
+            points[1] = new Point(10, 1);
+            points[2] = new Point(8, 6);
+            points[3] = new Point(5, 6);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.GeneralTrapezoid, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForSquare()
+        {
+            var points = new Point[4];
+            points[0] = new Point(0, 0);
+            points[1] = new Point(0, 2);
+            points[2] = new Point(2, 0);
+            points[3] = new Point(2, 2);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.Square, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForRectangle()
+        {
+            var points = new Point[4];
+            points[0] = new Point(-4, -2);
+            points[1] = new Point(-4, 2);
+            points[2] = new Point(5, 2);
+            points[3] = new Point(5, -2);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.Rectangle, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForParallelogram()
+        {
+            var points = new Point[4];
+            points[0] = new Point(2, 5);
+            points[1] = new Point(5, 2);
+            points[2] = new Point(10, 2);
+            points[3] = new Point(7, 5);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.Parallelogram, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForRhombus()
+        {
+            var points = new Point[4];
+            points[0] = new Point(0, 6);
+            points[1] = new Point(5, 3);
+            points[2] = new Point(10, 6);
+            points[3] = new Point(5, 9);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.Rhombus, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForIsoscelesTrapezoid()
+        {
+            var points = new Point[4];
+            points[0] = new Point(0, 0);
+            points[1] = new Point(3, 5);
+            points[2] = new Point(11, 5);
+            points[3] = new Point(14, 0);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.IsoscelesTrapezoid, figure.GetTypeFigure());
+        }
+
+        [Test]
+        public void PathForRectangularTrapezoid()
+        {
+            var points = new Point[4];
+            points[0] = new Point(3, 3);
+            points[1] = new Point(10, 3);
+            points[2] = new Point(9, 9);
+            points[3] = new Point(3, 9);
+
+            var figure = new Figure(points);
+            Assert.AreEqual(TypeFigure.RectangularTrapezoid, figure.GetTypeFigure());
         }
     }
 }
--- WhiteBox/ShadowTest.cs
+++ WhiteBox/ShadowTest.cs
@@ -1,7 +1,49 @@
-﻿namespace WhiteBox
+﻿using NUnit.Framework;
+using ShadowLab;
+
+namespace WhiteBox
 {
     public class ShadowTest
     {
-        
+        [Test]
+        public void PathForPoint()
+        {
+            var shadow = new Shadow();
+            var segment = new Segment(0, 0);
+            
+            Assert.IsFalse(shadow.AddSegment(segment));
+        }
+
+        [Test]
+        public void PathForInvalidInterval()
+        {
+            var shadow = new Shadow();
+            var segment = new Segment(10, 0);
+            
+            Assert.IsFalse(shadow.AddSegment(segment));
+            Assert.AreEqual(0,shadow.GetLength());
+        }
+
+        [Test]
+        public void PathForLength()
+        {
+            var shadow = new Shadow();
+            var segment = new Segment(0, 5);
+            
+            Assert.IsTrue(shadow.AddSegment(segment));
+            Assert.AreEqual(5, shadow.GetLength());
+        }
+
+        [Test]
+        public void PathIntersection()
+        {
+            var root = new Segment(0, 5);
+            var child = new Segment(2, 4);
+            var shadow = new Shadow();
+            
+            Assert.IsTrue(root.Intersection(child));
+            Assert.IsTrue(shadow.AddSegment(root) && shadow.AddSegment(child));
+            Assert.AreEqual(5, shadow.GetLength());
+        }
     }
 }
